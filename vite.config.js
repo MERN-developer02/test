66b60cf784c825c4564bba9e366d85122fdf32a8
subhadiproy3909 <subhadiproy3202@gmail.com>
2024-02-13 20:02:39 +0530

    netlify commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://test-server-tau-sepia.vercel.app/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
