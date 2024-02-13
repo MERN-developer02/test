4eab07fe23cb34b4fbfca74391fc360eaa625820
subhadiproy3909 <subhadiproy3202@gmail.com>
2024-02-13 17:39:49 +0530

    update again 3 .toml

--- vite.config.js
+++ vite.config.js
@@ -20,24 +20,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    },
-  },
-  optimizeDeps: {
-    // include: ['linked-dep'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // include: [/linked-dep/, /node_modules/],
     },
   },
 });
