e12b12c834eb9e59d2add4837184b4abe017fa7f
subhadiproy3909 <subhadiproy3202@gmail.com>
2024-02-13 16:16:30 +0530

    commit 6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000",
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
@@ -13,5 +31,13 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
-})
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
+});
