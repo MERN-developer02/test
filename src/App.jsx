e907526f0525c3e26f00369cf1f499d59bd13cd2
subhadiproy3909 <subhadiproy3202@gmail.com>
2024-02-13 14:15:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [v, setV] = useState(null);
+
+  useEffect(() => {
+    // if(v === null) return;
+
+    fetch("api/", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      }
+    })
+      .then((data) => {
+        data.json().then((value) => setV(value));
+        // data.json();
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
+  console.log(v);
+  return (
+    <>
+      <div>hello {v?.name}</div>
+    </>
+  )
+}
+
+export default App
